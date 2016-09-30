664834b11145f22de75c8edf4785cb099b6f5b63
Alton Lau <altondev@gmail.com>
2016-09-29 22:45:36 -0400

    Initial Commit

---
+++ Foodie/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Foodie
+//
+//  Created by Alton Lau on 2016-09-29.
+//  Copyright © 2016 Alton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
